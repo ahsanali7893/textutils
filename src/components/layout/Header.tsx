2c03c17e5a0306d87d4c17017be9179c0c300deb
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-20 12:00:05 +0500

    initial changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 function Header() {
   return (
     <><header className="text-gray-600 body-font">
@@ -9,10 +11,8 @@ function Header() {
         <span className="ml-3 text-xl">Tailblocks</span>
       </a>
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        <a className="mr-5 hover:text-gray-900">First Link</a>
-        <a className="mr-5 hover:text-gray-900">Second Link</a>
-        <a className="mr-5 hover:text-gray-900">Third Link</a>
-        <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+        <Link to={'/'} className="mr-5 hover:text-pink-900">Home</Link>
+        <Link to={'/about'} className="mr-5 hover:text-pink-900">About</Link>
       </nav>
       <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
