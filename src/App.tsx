bfa7a352b57bcf45d36e44fe8d122d97f19de14a
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-20 12:16:33 +0500

    Initial changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Header from './components/layout/Header';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
+   <BrowserRouter>
      <Header/>
+     </BrowserRouter>
     </>
   );
 }
