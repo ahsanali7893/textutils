bae35d4ff57243cc09a6a48c32a7b0a7052398fc
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-20 13:10:15 +0500

    initial changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './components/layout/Header';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import TextUtils from './components/layout/TextUtils';
 
 function App() {
   return (
     <>
    <BrowserRouter>
      <Header/>
+     <TextUtils/>
      </BrowserRouter>
     </>
   );
---
+++ src/components/layout/TextUtils.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TextUtils = () => {
+  return (
+    <><div className="relative mb-4">
+    <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+    <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+  </div></>
+  )
+}
+
+export default TextUtils
