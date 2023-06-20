ce84754f9425f2b86efe985a4ff9f1a81887e929
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-20 13:45:53 +0500

    Initial changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,17 +2,19 @@ import { Link } from "react-router-dom"
 
 function Header() {
   return (
-    <><header className="text-gray-600 body-font">
-    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <Link to={'/'} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <span className="ml-3 text-xl">TextUtils</span>
-      </Link>
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        <Link to={'/'} className="mr-5 hover:text-pink-900">Home</Link>
-        <Link to={'/about'} className="mr-5 hover:text-pink-900">About</Link>
-      </nav>
-    </div>
-  </header></>
+    <>
+      <header className="text-gray-600 body-font max-w-screen-lg mx-auto">
+        <div className="container mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
+          <Link to={'/'} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span className="text-xl">TextUtils</span>
+          </Link>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <Link to={'/'} className="mr-5 hover:text-pink-900">Home</Link>
+            <Link to={'/about'} className="hover:text-pink-900">About</Link>
+          </nav>
+        </div>
+      </header>
+    </>
   )
 }
 
--- src/components/layout/TextUtils.tsx
+++ src/components/layout/TextUtils.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TextUtils = () => {
+
+  const [text, settext] = useState('');
+
+  const uppercase = () => {
+    let newtext = text.toUpperCase();
+    settext(newtext);
+      // showalert(" UpperCase converted ", "success");
+  }
   return (
-    <><div className="relative mb-4">
-    <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
-    <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-  </div></>
-  )
+    <div className='max-w-screen-lg mx-auto'>
+    <div className="relative mb-4 ">
+    <label htmlFor="message" className="leading-7 text-sm text-gray-600">TextUtils</label>
+    <textarea value={text} onChange={(e:any)=>{settext(e.target.value)}} id="textbox" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"/>
+  </div>
+    <button onClick={uppercase} className="text-white mx- bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-sm">UpperCase</button>
+    </div>
+  );
 }
 
-export default TextUtils
+export default TextUtils;
+
