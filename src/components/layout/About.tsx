91ff23d7906fd57f9cac1f8d716a7535cb63fdac
ahsanali7893 <ahsan3916902@gmail.com>
2023-07-20 15:26:28 +0500

    initial changes

---
+++ src/components/layout/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {}
+
+const About = (props: Props) => {
+  return (
+    <div>
+        <div className="container">
+            <div>
+            </div>
+
+            <div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
